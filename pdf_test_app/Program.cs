a74deb52187baa853feceb0cc5ac3a6f27214603
unknown <Santhosh@.(none)>
2011-12-25 00:52:02 +0530

    added supported files

---
+++ pdf_test_app/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+namespace pdf_test_app
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            const string newFile = @"C:\Users\Santhosh\AppData\test.pdf";
+            var doc = new Document();
+            PdfWriter writer = PdfWriter.GetInstance(doc, new FileStream(newFile, FileMode.Create));
+            // Metadata
+            doc.AddCreator("PDF Printer by Joachim Tesznar");
+            doc.AddTitle("New PDF Document");
+            // Add content
+            doc.Open();
+            doc.Add(new Paragraph("Hello World"));
+            doc.Close();
+        }
+    }
+}
